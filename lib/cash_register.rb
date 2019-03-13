11b2cb87f925bb700606e362c651b0d76f07bab9
Oliver <oliver.grant@gmail.com>
2019-03-13 16:12:34 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,9 +18,9 @@ end
 def apply_discount
   if @discount > 0
     @total = @total - (@total * @discount/100)
-    puts "After the discount, the total comes to #{@total}."
+    "After the discount, the total comes to #{@total}."
   else 
-    puts "There is no discount to apply."
+    "There is no discount to apply."
   end
 end
 
