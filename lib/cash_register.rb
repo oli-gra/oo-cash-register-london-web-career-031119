a3191c2cf151e5a7e8b8b92377dd16b63e735b55
Oliver <oliver.grant@gmail.com>
2019-03-13 15:58:40 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,22 @@
+require 'pry'
 
+class CashRegister
+  
+  attr_accessor :discount, :total
+
+def initialize (discount=0)
+  @total = 0
+  @discount = discount
+end
+
+def add_item (item,price,quantity=1)
+  @total += price * quantity
+end
+
+def apply_discount
+  binding.pry
+  @total = @total * (@discount / 100)
+  puts "After the discount, the total comes to #{@total}."
+end
+
+end
